44ec53db163dd79789790fbe93c552ab27a81352
zhaozhenfei <353149695@qq.com>
2024-05-03 17:59:59 +0800

    Add QuickSort

---
+++ src/main/java/cartoon/QuickSort.java
@@ -0,0 +1,41 @@
+package cartoon;
+
+/**
+ * Created by zhenfei7 on 2024/4/23.
+ */
+public class QuickSort {
+
+    private static int partition(int[] arr, int startIndex, int endIndex) {
+        int pivot = arr[startIndex];
+        int mark = startIndex;
+        for (int i = startIndex + 1; i <= endIndex; i++) {
+            if (arr[i] < pivot) {
+                mark++;
+                int temp = arr[mark];
+                arr[mark] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        arr[startIndex] = arr[mark];
+        arr[mark] = pivot;
+        return mark;
+    }
+
+    public static void quickSort(int[] arr, int startIndex, int endIndex) {
+        if (startIndex >= endIndex) {
+            return;
+        }
+        int pivot = partition(arr, startIndex, endIndex);
+        quickSort(arr, startIndex, pivot - 1);
+        quickSort(arr, pivot + 1, endIndex);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 1, 4, 2, 5, 6, 11, 8, 9};
+        quickSort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
