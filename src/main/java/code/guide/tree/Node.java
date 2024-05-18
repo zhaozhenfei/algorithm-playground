5fc77e45ad6d63c705d7f536135118ce1f858681
zhaozhenfei <353149695@qq.com>
2024-05-18 22:40:14 +0800

    Add code guide tree Solution06

---
+++ src/main/java/code/guide/tree/Node.java
@@ -0,0 +1,15 @@
+package code.guide.tree;
+
+/**
+ * Created by zhenfei7 on 2024/5/18.
+ */
+
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/code/guide/tree/Solution06.java
@@ -0,0 +1,52 @@
+package code.guide.tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zhenfei7 on 2024/5/18.
+ */
+public class Solution06 {
+    public static void main(String[] args) {
+        Node head = new Node(-3);
+        head.left = new Node(3);
+        head.right = new Node(-9);
+        head.left.left = new Node(1);
+        head.left.right = new Node(0);
+        head.left.right.left = new Node(1);
+        head.left.right.right = new Node(6);
+        head.right.left = new Node(2);
+        head.right.right = new Node(1);
+        System.out.println(getMaxLength(head, 6));
+//        System.out.println(getMaxLength(head, -9));
+//        System.out.println(getMaxLength(head, 9));
+    }
+
+    public static int getMaxLength(Node head, int sum) {
+        Map<Integer, Integer> sumMap = new HashMap<>();
+        sumMap.put(0, 0);
+        return preOrder(head, sum, 0, 1, 0, sumMap);
+    }
+
+    public static int preOrder(Node head, int sum, int preSum, int level, int maxLen, Map<Integer, Integer> sumMap) {
+        if (head == null) {
+            return maxLen;
+        }
+        int curSum = preSum + head.value;
+        if (!sumMap.containsKey(curSum)) {
+            sumMap.put(curSum, level);
+        }
+        if (sumMap.containsKey(curSum - sum)) {
+            maxLen = Math.max(level - sumMap.get(curSum - sum), maxLen);
+        }
+
+        maxLen = preOrder(head.left, sum, curSum, level + 1, maxLen, sumMap);
+        maxLen = preOrder(head.right, sum, curSum, level + 1, maxLen, sumMap);
+
+        //重点理解：因为要返回父节点了，要把当前层的路径和删掉
+        if (level == sumMap.get(curSum)) {
+            sumMap.remove(curSum);
+        }
+        return maxLen;
+    }
+}
